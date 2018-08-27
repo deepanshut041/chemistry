4c1e6ac902ff14db60b4129ba4f15ffe69cdef6e
deepanshut041 <deepanshut041@gmail.com>
2018-08-27 21:51:16 +0530

    Create basic authentication models User and Role with Auditing date

--- src/main/java/in/deepanshutyagi/chemistry/ChemistryApplication.java
+++ src/main/java/in/deepanshutyagi/chemistry/ChemistryApplication.java
@@ -3,6 +3,8 @@ package in.deepanshutyagi.chemistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ChemistryApplication {
 
---
+++ src/main/java/in/deepanshutyagi/chemistry/model/enums/RoleName.java
@@ -0,0 +1,6 @@
+package in.deepanshutyagi.chemistry.model.enums;
+
+public enum  RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
