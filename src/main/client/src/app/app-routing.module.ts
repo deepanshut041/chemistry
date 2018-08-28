c0664d0802774ab38d7204dd67d6f355da1f15e8
deepanshut041 <deepanshut041@gmail.com>
2018-08-29 01:59:29 +0530

    Add bsic navbar and footer to project

--- src/main/client/src/app/app-routing.module.ts
+++ src/main/client/src/app/app-routing.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 import {MainComponent} from "./main/main.component";
+import {HomeComponent} from "./main/home/home.component";
 
 
 const routes:Routes =[
-  {path:'portal', loadChildren:"./admin/admin.module#AdminModule"},
+  {path:'admin', loadChildren:"./admin/admin.module#AdminModule"},
   {path:'user', loadChildren:"./user/user.module#UserModule"},
   {
-    path: '', component: MainComponent
+    path: '', component: MainComponent, children:[
+      {path: '', component: HomeComponent}
+    ]
   },
   // {
   //   path: 'auth', component: AuthComponent, children: [
--- src/main/client/src/app/main/main.module.ts
+++ src/main/client/src/app/main/main.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 import { MainComponent } from './main.component';
 import {CommonModule} from "@angular/common";
 import {RouterModule} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
+import {NavbarComponent} from "./navbar/navbar.component";
+import {FooterComponent} from "./footer/footer.component";
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
   declarations: [
-    MainComponent
+    MainComponent, HomeComponent, NavbarComponent, FooterComponent
   ],
   imports: [
-    CommonModule, RouterModule
+    CommonModule, RouterModule, MDBBootstrapModule
   ],
   providers: []
 })
