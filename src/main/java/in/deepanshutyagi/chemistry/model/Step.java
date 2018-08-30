49a8a7fabe78607e594e368ed6c3bdacbd7a82d9
deepanshut041 <deepanshut041@gmail.com>
2018-08-30 16:46:47 +0530

    Added basic step structure

--- src/main/java/in/deepanshutyagi/chemistry/model/Step.java
+++ src/main/java/in/deepanshutyagi/chemistry/model/Step.java
@@ -1,5 +1,8 @@
 package in.deepanshutyagi.chemistry.model;
 
+import in.deepanshutyagi.chemistry.model.enums.StepType;
+import org.hibernate.annotations.NaturalId;
+
 import javax.persistence.*;
 
 @Entity
@@ -9,4 +12,67 @@ public class Step {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String title;
+
+    @Enumerated(EnumType.STRING)
+    @NaturalId
+    @Column(length = 20)
+    private StepType type;
+
+    private String videoLink;
+    
+    private String text;
+
+    private Module module;
+
+    public Step() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public StepType getType() {
+        return type;
+    }
+
+    public void setType(StepType type) {
+        this.type = type;
+    }
+
+    public String getVideoLink() {
+        return videoLink;
+    }
+
+    public void setVideoLink(String videoLink) {
+        this.videoLink = videoLink;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Module getModule() {
+        return module;
+    }
+
+    public void setModule(Module module) {
+        this.module = module;
+    }
 }
